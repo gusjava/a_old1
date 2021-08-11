91ad0943199a22b772fb8261ca1380594c0ad935
Augustin Delale <adelale@hotmail.com>
2021-08-11 23:04:37 +0200

    formatting framework

--- src/a/framework/Manager.java
+++ src/a/framework/Manager.java
@@ -3,5 +3,5 @@ package a.framework;
 public interface Manager {
     public Service service(Entity entity, String id) throws Exception; 
     public Object resource(Entity entity, String id) throws Exception;
-    public void err(Entity entity, String id, Exception e);
+    public void err(Entity entity, String id, Exception e); 
 }
--- src/a/framework/Service.java
+++ src/a/framework/Service.java
@@ -1,4 +1,3 @@
 package a.framework;
 
-public interface Service extends B, E, F, G, H, I, P, R, S, T, V {
-}
+public interface Service extends B, E, F, G, H, I, P, R, S, T, V {}
