e294b124e41b37a24f5a83247be6962f891defa5
Augustin Delale <adelale@hotmail.com>
2021-08-13 11:41:38 +0200

    gyem 20210813

--- src/a/core/gus/gyem/m003/g/prop/Module.java
+++ src/a/core/gus/gyem/m003/g/prop/Module.java
@@ -1,7 +1,9 @@
 package a.core.gus.gyem.m003.g.prop;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
+
 import a.core.gus.gyem.GyemSystem;
 import a.framework.G;
 
@@ -19,5 +21,15 @@ public class Module extends GyemSystem implements G {
 		
 		Map inside = (Map) moduleG(M004_G_PROP_CONFIG).g();
 		if(inside!=null) prop.putAll(inside);
+		
+		Map param = (Map) moduleG(M010_G_PARAM).g();
+		Iterator it = param.keySet().iterator();
+		while(it.hasNext()) {
+			String key = (String) it.next();
+			if(key.startsWith("p.")) {
+				String value = (String) param.get(key);
+				prop.put(key.substring(2), value);
+			}
+		}
 	}
 }
